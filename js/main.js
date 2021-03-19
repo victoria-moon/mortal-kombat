61c24af97e02d243662c9357abe7664a7dc149ff
victoria-moon <79410720+victoria-moon@users.noreply.github.com>
2021-03-19 13:53:42 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('#app').vide({
+    mp4: 'video source/video.mp4',
+    webm: 'video source/video.webm',
+    ogv: 'video source/video.ogv',
+    poster: 'img/cover-image.jpg'
+    }); 
+});
