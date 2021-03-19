8890e547451e61a062faaab0ee25602bb5c80d16
victoria-moon <79410720+victoria-moon@users.noreply.github.com>
2021-03-19 14:42:34 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
     $('#app').vide({
     mp4: 'video source/video.mp4',
     webm: 'video source/video.webm',
-    ogv: 'video source/video.ogv',
-    poster: 'img/cover-image.jpg'
+    ogv: 'video source/video.ogv'
     }); 
 });
